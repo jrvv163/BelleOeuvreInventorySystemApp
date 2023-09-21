806f5f99913ae88b2226f6427339628f745f06c4
ARJAY <ARJAY@DESKTOP-QHDSJRV>
2023-09-21 11:21:21 +0800

    Add project files.

---
+++ Page Navigation App/Model/PageModel.cs
@@ -0,0 +1,15 @@
+﻿using System;
+
+namespace Page_Navigation_App.Model
+{
+    public class PageModel
+    {
+        public int CustomerCount { get; set; }
+        public string ProductStatus { get; set; }
+        public DateOnly OrderDate { get; set; }
+        public decimal TransactionValue { get; set; }
+        public TimeOnly ShipmentDelivery { get; set; }
+        public bool LocationStatus { get; set; }
+
+    }
+}
---
+++ Page Navigation App/View/Home.xaml.cs
@@ -0,0 +1,15 @@
+﻿using System.Windows.Controls;
+
+namespace Page_Navigation_App.View
+{
+    /// <summary>
+    /// Interaction logic for Home.xaml
+    /// </summary>
+    public partial class Home : UserControl
+    {
+        public Home()
+        {
+            InitializeComponent();
+        }
+    }
+}
